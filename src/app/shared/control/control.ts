86cb5c5f2e148949234ce86094329a3adec8b0fc
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-27 20:06:46 +0330

    Sperate css and use :host

--- src/app/shared/control/control.ts
+++ src/app/shared/control/control.ts
@@ -1,11 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.html',
   styleUrl: './control.css',
+  encapsulation: ViewEncapsulation.None,
+   host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class Control {
 label = input.required<string>();
+el = inject(ElementRef);
+  onClick() {
+    console.log('clicked');
+    console.log(this.el);
+  }
 }
