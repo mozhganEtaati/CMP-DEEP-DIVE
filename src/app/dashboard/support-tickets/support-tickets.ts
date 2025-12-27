937d61f377e19575f29344ec97efda347a5bb6b2
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-27 16:13:48 +0330

    Add shared button attribute component

--- src/app/dashboard/support-tickets/support-tickets.ts
+++ src/app/dashboard/support-tickets/support-tickets.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Ticket } from "./ticket/ticket";
 
 @Component({
   selector: 'app-support-tickets',
-  imports: [],
+  imports: [Ticket],
   templateUrl: './support-tickets.html',
   styleUrl: './support-tickets.css',
 })
