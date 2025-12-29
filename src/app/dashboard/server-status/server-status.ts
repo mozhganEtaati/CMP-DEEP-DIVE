0ce36fbc8654755fd8a82ea749e00f4d5ca34801
mozhganEtaati <m.etaati73@yahoo.com>
2025-12-29 19:52:17 +0330

    Use ngOnInit and onDestroy

--- src/app/dashboard/server-status/server-status.ts
+++ src/app/dashboard/server-status/server-status.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, DestroyRef, inject, Input, input, OnDestroy, OnInit } from '@angular/core';
 import { required } from '@angular/forms/signals';
 
 @Component({
@@ -7,6 +7,25 @@ import { required } from '@angular/forms/signals';
   templateUrl: './server-status.html',
   styleUrl: './server-status.css',
 })
-export class ServerStatus {
-  currentStatus = 'online';
+export class ServerStatus implements OnInit {
+
+  currentStatus : 'online'|'offline'|'unknown'='offline';
+  destroyRef= inject(DestroyRef);
+  ngOnInit(){
+    const intervalId = setInterval(()=>{
+      const rnd = Math.random();
+      if(rnd < 0.5){
+        this.currentStatus ='online';
+      }
+      else if(rnd <0.9){
+        this.currentStatus ='offline'
+      }else{
+        this.currentStatus = 'unknown'
+      }
+    },5000);
+
+    this.destroyRef.onDestroy(()=>{
+      clearInterval(intervalId);
+    });
+  }
 }
